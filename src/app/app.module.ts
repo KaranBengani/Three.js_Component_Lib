f3761ba2eef57af97d17ea79c281e88f9a6393f9
karan <karanbengani14@gmail.com>
2023-10-31 00:20:26 +0530

    basic repository setup complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CubeComponent } from './basicShapes/cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule,
